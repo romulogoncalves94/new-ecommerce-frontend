6518d2487a6a098196a3a1c31c8d5c07423c3468
romulogoncalves94 <romulogoncalves94@hotmail.com>
2024-09-02 07:52:41 -0300

    Desenvolvido para mostrar todos os produtos para um usuário customer.

--- src/app/customer/components/dashboard/dashboard.component.ts
+++ src/app/customer/components/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {CustomerService} from "../../services/customer.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +10,48 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  products: any[] = [];
+  searchProductForm!: FormGroup;
+
+  constructor(
+    private customerService: CustomerService,
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    this.loadForm();
+    this.getAllProducts();
+  }
+
+  loadForm() {
+    this.searchProductForm = this.fb.group({
+      title: [null, Validators.required]
+    })
+  }
+
+  getAllProducts() {
+    this.products = []
+    this.customerService.getAllProducts().subscribe(res => {
+      res.forEach(element => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.byteImg
+        this.products.push(element);
+      });
+    });
+  }
+
+  submitForm() {
+    this.products = []
+    const title = this.searchProductForm.get('title')!.value;
+    this.customerService.getAllProductByName(title).subscribe(res => {
+      res.forEach(element => {
+        element.processedImg = 'data:image/jpeg;base64,' + element.byteImg
+        this.products.push(element);
+      });
+    });
+  }
+
+  addToCart(id) {
+
+  }
 }
--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -1,9 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CustomerRoutingModule } from './customer-routing.module';
-import { CustomerComponent } from './customer.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {CustomerRoutingModule} from './customer-routing.module';
+import {CustomerComponent} from './customer.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
 
 
 @NgModule({
@@ -13,7 +20,16 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   ],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule
   ]
 })
-export class CustomerModule { }
+export class CustomerModule {
+}
